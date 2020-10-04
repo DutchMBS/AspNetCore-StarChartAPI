cf4ab8ce6ea9c9e335303ceec157576a742945da
Michiel Boulonois <michiel@boulonois.nl>
2020-10-04 23:16:08 +0200

    Update ApplicationDbContext.cs
    W

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<CelestialObject> CelestialObjects { get; set; }
+        public DbSet<CelestialObject> Satellites { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
