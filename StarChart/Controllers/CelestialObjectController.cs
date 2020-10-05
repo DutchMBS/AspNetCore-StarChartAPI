9981412d6d497e05a3495583115a06e65d35c8d4
Michiel Boulonois <michiel@boulonois.nl>
2020-10-05 23:50:17 +0200

    Update CelestialObjectController.cs
    5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
@@ -17,5 +18,45 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+
+            var T = _context.CelestialObjects.Find(id);
+            if (T != null)
+            {
+                T.Satellites.Add(T);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var T = _context.CelestialObjects.Find(name);
+            if (T != null)
+            {
+                T.Satellites.Add(T);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            foreach (var item in _context.CelestialObjects)
+            {
+                item.Satellites.Add(item);
+
+            }
+            return Ok();
+        }
     }
 }
